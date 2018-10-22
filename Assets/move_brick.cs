3c9de51890bc38a76337aaf5dbfff98d6183f219
Ryazantsev Gleb <dev.ryazantsev@gmail.com>
2018-10-22 23:05:46 +0700

    Add buttons and move brick

---
+++ Assets/move_brick.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class move_brick : MonoBehaviour {
+
+
+
+    public Button left_btn;
+    public Button right_btn;
+
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                //print("space key was pressed");
+                transform.position = new Vector3(transform.position.x - 0.2f, transform.position.y, transform.position.z);
+            }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            print("space key was pressed");
+            transform.position = new Vector3(transform.position.x + 0.2f, transform.position.y, transform.position.z);
+        }
+
+        // print("AYR");
+    }
+}
